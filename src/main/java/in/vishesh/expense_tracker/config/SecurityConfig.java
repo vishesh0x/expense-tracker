f97b9f04a6fc928fda15a6e271c021dae9588588
Vishesh Kumar <67827886+vishesh0x@users.noreply.github.com>
2025-11-18 17:32:33 +0530

    Update SecurityConfig.java

--- src/main/java/in/vishesh/expense_tracker/config/SecurityConfig.java
+++ src/main/java/in/vishesh/expense_tracker/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://et-expense-tracker.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
